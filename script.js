d35948a0634db92ed58868155f8311003a019d23
Carson Senthilkumar <77632579+CarsonSSk@users.noreply.github.com>
2023-02-05 13:02:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+document.getElementById("button5").addEventListener("click", function(){
+        
+    const myTimeout = setTimeout(reminder, 3600000);
+
+        function reminder() {
+            alert("Time to take your 5 minute break.")
+        }
+}
+)
+
+document.getElementById("button10").addEventListener("click", function(){
+    
+    const myTimeout = setTimeout(reminder, 3600000);
+
+        function reminder() {
+            alert("Time to take your 10 minute break.")
+        }
+}
+)
+
+document.getElementById("button15").addEventListener("click", function(){
+    
+    const myTimeout = setTimeout(reminder, 3600000);
+
+        function reminder() {
+            alert("Time to take your 15 minute break.")
+        }
+}
+)
+
+
+
